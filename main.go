5408304e5ef0ef6d750942c7458ba5b54b62cdb7
gunni <gunni1111@gmail.com>
2017-08-22 22:57:26 +0200

    main file

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"github.com/gorilla/mux"
+	"fmt"
+)
+
+const (
+	port = ":7000"
+)
+
+func main() {
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", index)
+
+	fmt.Println("listening on " + port)
+	log.Fatal(http.ListenAndServe(port, router))
+}
+
+func index(response http.ResponseWriter, request *http.Request) {
+	fmt.Fprintln(response, "Hello")
+}
